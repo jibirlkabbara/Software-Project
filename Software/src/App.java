6bacbeaf090e4ed0feb4363cda702a7b8c398027
BaderOth <bader0198@gmail.com>
2024-04-22 13:10:11 +0300

    Multiplication Function

--- Software/src/App.java
+++ Software/src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int y = in.nextInt();
+
+        System.out.println(multiplication(x, y));
+    }
+
+    public static int multiplication(int x, int y) {
+        int result = 1;
+        if (x < y) {
+            for (int i = x; i <= y; i++) {
+                result *= i;
+            }
+        } else if (x > y) {
+            for (int i = y; i <= x; i++) {
+                result *= i;
+            }
+        } else {
+            return x;
+        }
+        return result;
     }
 }
