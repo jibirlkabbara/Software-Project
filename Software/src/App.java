a7b70eac247ff25b8c51ce1fcd79c1ab38089968
AbdulrahmanHalabi <halabiabdulrahman5@gmail.com>
2024-04-22 13:09:02 +0300

    Addition function

--- Software/src/App.java
+++ Software/src/App.java
@@ -1,5 +1,26 @@
 public class App {
+    public static int add(int x, int y) {
+        int sum = 0;
+        if (x > y) {
+            for (int i = y; i <=x; i++) {
+
+                sum += i;
+
+            }
+            return sum; }
+      else if (x < y) {
+            for (int i = x; i <=y; i++) {
+
+                sum += i;
+
+            }
+       return sum; }
+       return 0;
+        
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+      
     }
 }
